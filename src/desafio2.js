4d0a9255e74d642ab4455ad1ea6aeed7002c11ce
yourik1 <gabrielm.g-010@hotmail.com>
2024-02-06 23:21:33 -0300

    add comentários no desafio2

--- src/desafio2.js
+++ src/desafio2.js
@@ -1,3 +1,4 @@
+// Const para o limite de cada elo
 const limiteFerro = 10;
 const limiteBronze = 20;
 const limitePrata = 50;
@@ -5,12 +6,12 @@ const limiteOuro = 80;
 const limiteDiamante = 90;
 const limiteLendario = 100;
 
-
+// Função que calcula o saldo de vitórias
 function calcularSaldoVitorias(vitorias, derrotas) {
-    return saldoVitorias = vitorias - derrotas;
-    
+    return saldoVitorias = vitorias - derrotas;   
 }
 
+// Função para ranquear o herói com base no saldo de vitórias
 function Rankear(){
     let saldoVitorias = calcularSaldoVitorias(70, 9);
 
@@ -32,7 +33,9 @@ function Rankear(){
         nivel = "Imortal";
     }
 
+    // Exibe o resultado
     console.log(`O herói tem de saldo de vitórias ${saldoVitorias} e está no nível de ${nivel}.`)
 }
 
+// Chamada da função para ranquear o herói
 Rankear();
